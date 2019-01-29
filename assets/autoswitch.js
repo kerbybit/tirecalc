ac21113d8043b3d43866b303da6a639498cd3ffd
Andrew Koerber <kerbybit@gmail.com>
2019-01-29 12:15:09 -0600

    updated autoswitch

--- assets/autoswitch.js
+++ assets/autoswitch.js
@@ -1,23 +1,73 @@
 $(document).ready(function() {
-    $(".tire-size").keyup(function () {
+    $(".tire-size").keyup(function (event) {
         $this=$(this);
-        if ($this.val().length >= $this.data("maxlength")) {
-            if($this.val().length > $this.data("maxlength")) {
-                $this.val($this.val().substring(0, $this.data("maxlength")));
-            }
-            var next = $this.next(".tire-size");
+		
+		if (event.keyCode == 37 
+		|| event.keyCode == 38 
+		|| event.keyCode == 39 
+		|| event.keyCode == 40
+		|| event.keyCode == 16
+		|| event.keyCode == 9) {
+			return
+		}
+		
+		if (event.keyCode == 8) {
+			if ($this.val().length == 0) {
+				var prev = $this.prev(".tire-size")
+				prev.focus()
+			}
+			return
+		}
+		
+		var next = $this.next(".tire-size")
+		
+		if ($this.val().length > $this.data("maxlength")) {
+			var extra = $this.val().substring($this.data("maxlength"), $this.val().length)
+			console.log(extra)
+			$this.val($this.val().substring(0, $this.data("maxlength")))
+			next.val(extra)
+			next.focus()
+			return
+		}
+		
+        if ($this.val().length == $this.data("maxlength")) {
             next.val("")
             next.focus()
         }
     });
 
     $(".tread-depth").keyup(function () {
-        $this=$(this);
-        if ($this.val().length >= $this.data("maxlength")) {
-            if($this.val().length > $this.data("maxlength")) {
-                $this.val($this.val().substring(0, $this.data("maxlength")));
-            }
-            var next = $this.next(".tread-depth");
+		$this=$(this);
+		
+		if (event.keyCode == 37 
+		|| event.keyCode == 38 
+		|| event.keyCode == 39 
+		|| event.keyCode == 40
+		|| event.keyCode == 16
+		|| event.keyCode == 9) {
+			return
+		}
+		
+		if (event.keyCode == 8) {
+			if ($this.val().length == 0) {
+				var prev = $this.prev(".tread-depth")
+				prev.focus()
+			}
+			return
+		}
+		
+		var next = $this.next(".tread-depth")
+		
+		if ($this.val().length > $this.data("maxlength")) {
+			var extra = $this.val().substring($this.data("maxlength"), $this.val().length)
+			console.log(extra)
+			$this.val($this.val().substring(0, $this.data("maxlength")))
+			next.val(extra)
+			next.focus()
+			return
+		}
+		
+        if ($this.val().length == $this.data("maxlength")) {
             next.val("")
             next.focus()
         }
